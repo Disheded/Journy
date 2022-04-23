97a528409264b72065f92d6ebe923098f16e389d
Disheded <97792049+Disheded@users.noreply.github.com>
2022-04-23 17:24:47 +0300

    final

--- send.php
+++ send.php
@@ -8,12 +8,11 @@
     $msg = trim(urldecode(htmlspecialchars($msg)));
 
     $html_text = "<h1>На вашем сайте была составлена заявка</h1>
-                    <br>от: <b>".$name."</b>
-                    <br>e-mail: <b>".$email."</b>
-                    <br>пользователь оставил комментарий ".$msg."
+                    <br>От: <b>".$name."</b>
+                    <br>E-mail: <b>".$email."</b>
+                    <br>Пользователь оставил комментарий ".$msg."
                     <br>Поздравляю с новой заявкой!";
-    $my_email = "ikowlad@gmail.com";
-
+    $my_email = "hgfgvvg.ru@gmail.com";
     if(mail($my_email, "Новый запрос на создание сайта", $html_text,
           "From: cccr-early.ru\r\n". "Content-type: text/html\r\n")){
         echo '{"status": "ok"}';
